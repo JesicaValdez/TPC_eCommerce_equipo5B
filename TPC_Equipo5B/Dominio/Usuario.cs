0fe5fcece92f2dc91230afaa9bef54d2c67d9554
JesicaValdez <jesica.valddez@gmail.com>
2024-10-21 21:47:36 +0100

    Commit clases
    Agrego Usuario y TipoUsuario en Dominio y conexión DB

---
+++ TPC_Equipo5B/Dominio/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public TipoUsuario TipoUsuario { get; set; }
+
+        public Usuario()
+        {
+            IdUsuario = 0;
+            NombreUsuario = string.Empty;
+            Email = string.Empty;
+            Password = string.Empty;
+            TipoUsuario = new TipoUsuario();
+        }
+
+        public Usuario(int idUsuario, string nombreUsuario, string apellido, string email, string password, TipoUsuario tipoUsuario)
+        {
+            IdUsuario = idUsuario;
+            NombreUsuario = nombreUsuario;
+            Email = email;
+            Password = password;
+            TipoUsuario = tipoUsuario;
+        }
+    }
+}
