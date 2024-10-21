1397e047c10415cb357558209ae58713b6489827
Yamil <yamil_diaz@hotmail.com>
2024-10-21 19:18:02 -0300

    commit
    Clases resuelta de Cliente y Carrito

---
+++ TPC_Equipo5B/Dominio/Carrito.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Carrito
+    {
+        List<Entrada> listaEntradas;
+
+        public void sumarEntradasAlCarrito(Entrada entrada)
+        {
+            listaEntradas.Add(entrada);
+        }
+
+        public int cantidadentradas()
+        {
+            return listaEntradas.Count();
+        }
+    }
+}
---
+++ TPC_Equipo5B/Dominio/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dominio
+{
+    public class Cliente
+    {
+        public int Id {  get; set; }
+        public string Dni {  get; set; }
+        public string Nombre { get; set; }
+        public string Apellido {  get; set; }
+        public string Email {  get; set; }
+        public string Direccion {  get; set; }
+        public string Ciudad {  get; set; }
+        public int CodigoPostal {  get; set; }
+
+        public Cliente()
+        {
+            Id = 0;
+            Dni = "";
+            Nombre = "";
+            Apellido = "";
+            Email = "";
+            Direccion = "";
+            Ciudad = "";
+            CodigoPostal = 0;
+        }
+    }
+}
